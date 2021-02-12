92198e45a4b53a7fbc4435eefcf5761eefd640b6
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2021-02-12 03:10:52 -0300

    feat: replace mat-paginator by app-paginator
    mat-paginator have a limitation about the 'next button', which, in this case, should be enable even if there's no additional data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,7 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { AfterViewInit, Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { IStarship } from '../core/model/starship.interface';
 import { StarWarsService } from '../core/star-wars.service';
-import { debounceTime } from 'rxjs/operators';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -14,14 +10,10 @@ import { debounceTime } from 'rxjs/operators';
 export class DashboardComponent implements AfterViewInit {
 
   userInputMGLT: number;
-  visitedPages: Object = {};
 
   displayedColumns: string[] = ['name', 'consumables', 'MGLT', 'totalStops'];
   dataSource: MatTableDataSource<IStarship>;
   public starShipsData = [];
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
 
   itemsPerPage: number = 10;
   currentPage: number = 1;
@@ -42,19 +34,13 @@ export class DashboardComponent implements AfterViewInit {
   }
 
   setStartshipsData() {
-    this.isChangingPage = true;
-
     this.starWarsService.getStarshipsData(this.currentPage)
       .then((res: any) => {
         this.existsNextPage = typeof res.next === 'string';
         this.existsPreviousPage = typeof res.previous === 'string';
 
         this.totalLength = res.count;
-        if (!this.visitedPages[this.currentPage]) {
-          this.visitedPages[this.currentPage] = res.results;
-        }
-
-        this.starShipsData = this.visitedPages[this.currentPage];
+        this.starShipsData = res.results;
 
         this.setDataTotalStops();
         this.initDataSource(this.starShipsData);
@@ -82,63 +68,48 @@ export class DashboardComponent implements AfterViewInit {
 
   initDataSource(data: IStarship[]) {
     this.dataSource = new MatTableDataSource();
-    this.dataSource.paginator = this.paginator;
     this.dataSource = new MatTableDataSource(data);
-    this.dataSource.sort = this.sort;
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+    this.setPage(1);
   }
 
   getMGLTValue(inputValueMGLT: number) {
     this.starShipsData = [];
     this.userInputMGLT = inputValueMGLT;
-    this.visitedPages = {};
-    this.dataSource.paginator.firstPage();
+
     this.setPage(1);
   }
 
   setPage(pageNumber: number) {
-    if (!this.isChangePageAvailable()) {
+    if (this.isChangingPage) {
       return;
     }
+
     this.currentPage = pageNumber;
     this.setStartshipsData()
   }
 
-  isChangePageAvailable() {
-    if (this.isChangingPage || !this.existsPreviousPage) {
-      return false;
-    }
-    return true;
-  }
-
-  changePageValidate() {
-    if (!this.isChangePageAvailable()) {
-      return;
-    }
-  }
-
   previousPage() {
-    if (!this.isChangePageAvailable()) {
+    if (this.isChangingPage || !this.existsPreviousPage) {
       return;
     }
 
+    this.isChangingPage = true;
     this.currentPage--;
     this.setStartshipsData()
   }
 
   nextPage() {
-    if (!this.existsNextPage) {
+    if (this.isChangingPage || !this.existsNextPage) {
       return;
     }
 
+    this.isChangingPage = true;
     this.currentPage++;
     this.setStartshipsData()
   }
